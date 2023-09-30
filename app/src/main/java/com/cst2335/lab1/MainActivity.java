ab49ab3aa28e227017deff8012ae3969b9319390
Zhile Wu <ninawu7824@gmail.com>
2023-09-30 15:33:50 -0400

    Step 2

--- app/src/main/java/com/cst2335/lab1/MainActivity.java
+++ app/src/main/java/com/cst2335/lab1/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_grid);
 
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(v -> Toast.makeText(getApplicationContext(),
